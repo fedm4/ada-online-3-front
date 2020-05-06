416be7be9342024794b8cf574619d036c34cdf61
Federico Mariño <fede.m.fede@gmail.com>
2020-05-05 21:59:51 -0300

    Clase 29

---
+++ Clase 29/archivo.js
@@ -0,0 +1,16 @@
+let pepe = 1;
+pepe = 2;
+console.log(pepe);
+
+// ///////////////////
+
+let variable1 = 1;
+const const1 = 1;
+
+variable1 = 2;
+
+const1 = 2;
+
+//  /////////////////////////////
+const SPADES = "spades";
+
