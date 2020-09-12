2182724e934d2cd4d6d4236ad00c4ebd6002612e
Federico Marino <fede.m.fede@gmail.com>
2020-09-12 11:12:16 -0300

    Clase 80

---
+++ Clase 80/proyectito001/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './components/Card/Card';
+import './App.css';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Card />
+      <Card />
+    </React.Fragment>
+  );
+}
+
+export default App;
